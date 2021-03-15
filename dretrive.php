dd07f6e3d71ec924cf4412b411f9e848894d5757
deena23 <deena@gmail.com>
2021-03-15 11:04:34 +0530

    first commit

---
+++ dretrive.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Music</title>
+    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="./fontawesome/css/all.css">
+    <link href="https://fonts.googleapis.com/css?family=Oswald&display=swap" rel="stylesheet">
+    <style>
+        table,th,td{
+            border: 3px solid rgba(12, 184, 182, 0.91);
+            border-collapse: collapse;
+            color:rgba(12, 184, 182, 0.91);
+        }
+         th{
+            background: rgba(12, 184, 182, 0.91);
+            color: white;
+            border: none;
+        }
+        th,td{
+            font-size: 17px;
+            width:100px;
+            padding: 20px;
+        }
+    </style>
+</head>
+<body>
+    <div class="menu-container">
+        <input type="checkbox" class="toggler">
+        <div class="hamburger">
+                <div> </div>
+        </div>
+        <div class="menu">
+            <div>
+                <div>
+                    <ul>
+                        <li><a href="index.html">Home</a></li>
+                        <li><a href="finddoctors.php">Find Doctors</a></li>
+                        <li><a href="retrive.php" style="color: rgba(12, 184, 182, 0.91);">Retrieve Medical Report</a></li>
+                        <li><a href="#">Find Hospital</a></li>
+                        <li><a href="#">Claiming Medical Insurance</a></li>
+                        <li><a href="#">Appointment</a></li>
+                        <li><a href="#">Medicine Order</a></li>
+                        <li><a href="#">Online Blood test</a></li>
+                        <li><a href="#">Check Blood Bank</a></li>
+                        <li><a href="#">Emergency Request</a></li>
+                        <li><a href="#">Pharmacy Availability</a></li>
+                        <li><a href="#">First Aid</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="back">
+        <div class="content">
+             <table border="1">
+            <tr align="justify">
+            <th> Name </th>
+            <th> Test </th>
+            <th> Disease </th>
+            <th> Phone </th>
+            <th> Hospital</th>
+        </tr>
+            <?php
+$servername = "localhost";
+$username = "root";
+$password = "23032000";
+$db = "web";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $db);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+// echo "Connected successfully";
+// $v = $_POST['option'];
+$sql = "SELECT * FROM retrieve";
+$result = $conn->query($sql);
+        if ($result->num_rows > 0)
+        {
+        // output data of each row
+        while($row = $result->fetch_assoc())
+            {
+                echo "<tr><td>" . $row["Name"]. "</td><td>" . $row["Test"]. "</td><td>" . $row["Disease"].
+                 "</td><td>" . $row["Phone"]. "</td><td>" . $row["Hospital"]."</td></tr>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
+        </div>
+    </div>
+</body>
+</html>
---
+++ retrive.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Music</title>
+    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="finddoctors.css">
+    <link rel="stylesheet" href="./fontawesome/css/all.css">
+    <link href="https://fonts.googleapis.com/css?family=Oswald&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div class="menu-container">
+        <input type="checkbox" class="toggler">
+        <div class="hamburger">
+                <div> </div>
+        </div>
+        <div class="menu">
+            <div>
+                <div>
+                    <ul>
+                        <li><a href="index.html">Home</a></li>
+                        <li><a href="finddoctors.php">Find Doctors</a></li>
+                        <li><a href="retrive.php" style="color: rgba(12, 184, 182, 0.91);">Retrieve Medical Report</a></li>
+                        <li><a href="#">Find Hospital</a></li>
+                        <li><a href="#">Claiming Medical Insurance</a></li>
+                        <li><a href="#">Appointment</a></li>
+                        <li><a href="#">Medicine Order</a></li>
+                        <li><a href="#">Online Blood test</a></li>
+                        <li><a href="#">Check Blood Bank</a></li>
+                        <li><a href="#">Emergency Request</a></li>
+                        <li><a href="#">Pharmacy Availability</a></li>
+                        <li><a href="#">First Aid</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="back">
+        <div class="content">
+            <h1>Find Doctor</h1>
+            <form action="dretrive.php" method="POST">
+            <button type="">Show Records</button>
+        </form>
+        </div>
+    </div>
+</body>
+</html>
